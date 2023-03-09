cf96aeb8e99f895bbae9e5deaa66535e814fa8ea
trotyl <mail@trotyl.xyz>
2023-03-09 11:00:53 -0500

    add connect page clipboard shortcut

---
+++ src/js/connect.js
@@ -0,0 +1,23 @@
+$(function () {
+    $('.discord').click(function () {
+        $('.discord>.toast').addClass("active")
+        var copyText = "Trotyl#0262";
+        copyToClipboard(copyText);
+        setTimeout(function() { $('.discord>.toast').removeClass("active"); }, 400);
+    })
+    $('.switch').click(function () {
+        $('.switch>.toast').addClass("active")
+        var copyText = "SW-0306-6465-4321";
+        copyToClipboard(copyText);
+        setTimeout(function() { $('.switch>.toast').removeClass("active"); }, 400);
+    })
+})
+
+function copyToClipboard(text) {
+    var sampleTextarea = document.createElement("textarea");
+    document.body.appendChild(sampleTextarea);
+    sampleTextarea.value = text; //save main text in it
+    sampleTextarea.select(); //select textarea contenrs
+    document.execCommand("copy");
+    document.body.removeChild(sampleTextarea);
+}
